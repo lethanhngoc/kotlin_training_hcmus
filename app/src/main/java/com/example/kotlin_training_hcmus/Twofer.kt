2d7bad281d7f83ec3cb8f1a551416d6542fa42c6
lethanhngoc <ngocle2071@gmail.com>
2020-04-24 22:39:59 +0700

    add solution Twofer

---
+++ app/src/main/java/com/example/kotlin_training_hcmus/Twofer.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin_training_hcmus
+
+internal fun twofer(name: String): String {
+    return "One for $name, one for me."
+}
+
+internal fun twofer(): String {
+    return return "One for you, one for me."
+}
